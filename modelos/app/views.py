b9758d668616e57fe2443745afd1fa4b75ffe126
svmalvaez <svmalvaez@gmail.com>
2015-04-16 17:40:00 -0500

    Push de emergencia

--- modelos/app/views.py
+++ modelos/app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-from app.models import Especie
+from app.models import Especie, Pato
 # Create your views here.
 
+def quiero_patos(request):
+	patos = Pato.objects.all()
+	print request.user
+	return render(request,"tus_patos.html",{"patos":patos})
